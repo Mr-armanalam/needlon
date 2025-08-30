7a48f849596ff4eaf167aa94f18d98bfa2cc9955
Arman Alam <armanalam78578@gmail.com>
2025-08-30 11:52:54 +0530

    wishlist url added

--- modules/shared/navbar/components/cart-and-wishlist.tsx
+++ modules/shared/navbar/components/cart-and-wishlist.tsx
@@ -1,26 +1,27 @@
 import { Bell, Heart, ShoppingBagIcon } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const CartAndWishList = () => {
   return (
     <div>
       <div className="flex items-center space-x-6">
-        <button className="flex cursor-pointer items-center space-x-2">
+        <Link href={`/wishlist`}  className="flex cursor-pointer items-center space-x-2">
           <Heart className="w-4 h-4" />
-        </button>
-        <button className="relative cursor-pointer">
+        </Link>
+        <Link href={'/your-cart'} className="relative cursor-pointer">
           <span className="absolute -top-2.5 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
             3
           </span>
           <ShoppingBagIcon className="w-4 h-4" />
-        </button>
-        <button className="flex cursor-pointer relative items-center space-x-2">
+        </Link>
+        <Link href={'/notification'} className="flex cursor-pointer relative items-center space-x-2">
           <span className="absolute -top-2.5 -right-4 bg-red-500 text-white text-xs rounded-full px-1">
             3
           </span>
 
           <Bell className="w-4 h-4 " />
-        </button>
+        </Link>
       </div>
     </div>
   );
