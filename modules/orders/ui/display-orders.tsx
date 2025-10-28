8b7293954239c40f78523238e742621c6695fc76
Arman Alam <armanalam78578@gmail.com>
2025-10-28 13:57:34 +0530

    some bug fixes in order pabe ui

--- modules/orders/ui/display-orders.tsx
+++ modules/orders/ui/display-orders.tsx
@@ -15,32 +15,37 @@ const DisplayOrders = ({ loading, orders }: props) => {
   console.log(orders);
 
   return (
-    <div className="mt-7">
+    <div className="mt-7 no-scrollbar">
       {loading ? (
         <p className="px-6">Loading orders...</p>
       ) : orders.length ? (
-        <div>
+        <div className="no-scrollbar ">
           {orders.length != 0 &&
             orders?.map((item, index) => (
               <div
                 key={index}
                 className={`border-y no-scrollbar  border-stone-200 mb-1.5 flex`}
               >
-                <div className="relative w-[180px] h-[180px]">
+                <div className="relative w-[190px] h-[190px]">
                   <Image src={item.image ?? ""} fill alt="wishlist items" />
                 </div>
                 <div className="relative w-full">
-                  <p className="text-xs px-6 py-4 border-b shadow-xs">
-                    ORDER PLACED
-                  </p>
                   <div className="my-6 ml-6 flex-1 text-stone-800">
                     <h1 className="font-semibold">{item.productName}</h1>
                     <p className="text-sm pl-0.5 lowercase text-stone-600">
                       Size: {item.size}
                     </p>
-                    <h2 className="text-xl font-semibold my-2">
+                    <h2 className="text-2xl font-semibold my-2">
                       ₹{item.price / 100}
                     </h2>
+                    <div className="flex gap-x-6">
+                      <Button className="text-xs rounded-full mt-3">
+                        Order placed
+                      </Button>
+                      <Button variant={"outline"} className="text-xs rounded-full mt-3">
+                        Prepaid
+                      </Button>
+                    </div>
                     <p className="absolute bottom-6 right-4 text-stone-500 text-xs ">
                       Delivery expected on {format(item.createdAt, "dd MMMM")}
                     </p>
