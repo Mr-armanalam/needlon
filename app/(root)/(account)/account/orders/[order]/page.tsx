763fced0264297e2c195e7f3631667dd6abf3820
Arman Alam <armanalam78578@gmail.com>
2025-12-02 10:10:38 +0530

    server side loading is done in individual order page

--- app/(root)/(account)/account/orders/[order]/page.tsx
+++ app/(root)/(account)/account/orders/[order]/page.tsx
@@ -1,12 +1,56 @@
-import IndividualOrder from '@/modules/orders/section/individual-order'
-import React from 'react'
+import { Address } from "@/features/address-slice";
+import IndividualOrder from "@/modules/orders/section/individual-order";
+import { cookies } from "next/headers";
+import React from "react";
 
-const page = () => {
-  return (
-    <div className=' overflow-y-scroll no-scrollbar'>
-      <IndividualOrder />
-    </div>
-  )
+export const dynamic = "force-dynamic";
+
+export interface OrderItemProp {
+  orderDate: Date;
+  orderId: string;
+  shippingAddress: Address;
+  paymentMode: string;
+  shippingCharge: number;
+  podCharge: number;
+  priceAtperchage: number;
+  productId: string;
+  image: string;
+  orderProperties?: string;
+  itemName: string;
+  couponDiscount: number;
+  totalPurchasePrice: number;
 }
 
-export default page
+async function getOrder(orderId: string, cookieStore: string) {
+  console.log(orderId, 'orderIdclient');
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}/api/orders/${orderId}`,
+    {
+      cache: "no-store",
+      headers: {Cookie: cookieStore },
+    }
+  );
+
+  if (!res.ok) return null;
+
+  const json = await res.json();
+  return json.order as OrderItemProp[];
+}
+
+const page = async ({ params }: { params: Promise<{ order: string }> }) => {
+  const { order: orderId } = await params;
+
+  const cookie = await cookies();
+  const cookieStore = cookie.toString();
+  const orderItem = await getOrder(orderId, cookieStore);
+
+  return (
+    <div className=" overflow-y-scroll no-scrollbar">
+      <IndividualOrder orderItem={orderItem} />
+    </div>
+  );
+};
+
+export default page;
+
