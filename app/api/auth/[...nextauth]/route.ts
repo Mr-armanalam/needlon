02cfc04f4c61d6170fff34a70d992463a1121d2a
Arman Alam <armanalam78578@gmail.com>
2026-01-01 19:52:48 +0530

    refactor: remove commented-out code from auth options for cleaner implementation

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,2 @@
-// import NextAuth from "next-auth";
-// import { authOptions } from "@/lib/auth-option/auth-data";
-
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
-
 import { handlers } from "@/auth";
 export const { GET, POST } = handlers
