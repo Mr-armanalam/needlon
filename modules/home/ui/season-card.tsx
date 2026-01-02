bf287833c3360bdfc408fe11292b7db4e1ed8751
Arman Alam <armanalam78578@gmail.com>
2026-01-03 05:24:25 +0530

    feat: update SeasonCard styles for improved layout and aesthetics; add new page component

--- modules/home/ui/season-card.tsx
+++ modules/home/ui/season-card.tsx
@@ -5,21 +5,21 @@ import React from "react";
 
 const SeasonCard = ({ productData }: { productData: ClientProductItem }) => {
   return (
-    <div className=" flex-1 min-w-[340px] flex flex-col py-10 bg-gradient-to-tl rounded-lg from-gray-950 to-gray-900">
-      <div className="relative mx-8 rounded-2xl box-border min-h-[300px] flex-1">
+    <div className=" flex-1 min-w-85 flex flex-col py-10 bg-linear-to-tl rounded-lg from-gray-950 to-gray-900">
+      <div className="relative mx-8 rounded-2xl box-border min-h-75 flex-1">
         <Image
           src={productData.image ?? "/images/image2.png"}
           fill
-          className="object-fill rounded-2xl h-[300px]"
+          className="object-fill rounded-2xl h-75"
           alt="season image"
         />
       </div>
-      <div className="text-white mt-6">
-        <div className="h-2 -skew-8 bg-white" />
-        <div className="relative px-6 pt-8 text-white">
+      <div className="text-white mt-2">
+        {/* <div className="h-2 -skew-8 bg-white" /> */}
+        <div className="relative px-8 pt-8 text-white">
           {/* Rating and Reviews */}
           <div className="flex text-[8px] items-center mb-3 gap-x-2">
-            <span className="mb-[1px]">{productData.averageRating}</span>
+            <span className="mb-px">{productData.averageRating}</span>
             <RatingDisplay
               color="#fff "
               size={9}
