fc167c1883877cd305c727b7c9a91fec16dc7ab4
Arman Alam <armanalam78578@gmail.com>
2026-01-25 21:39:46 +0530

    carousel card change

--- modules/product/ui/product-courusel.tsx
+++ modules/product/ui/product-courusel.tsx
@@ -13,21 +13,21 @@ import Image from "next/image";
 
 const ProductCourusel = () => {
   return (
-    <Carousel className="">
-      <CarouselContent className="pl-4">
+    <Carousel className="h-134">
+      <CarouselContent className="pl-1">
         {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="basis-1/3 pl-2 ol-1">
-              <Card className="rounded-xs bg-gray-100 border-0 py-0">
-                <CardContent className="flex relative  h-140 items-center justify-center p-6">
+          <CarouselItem key={index} className="basis-1/3 pl-4 ol-1">
+              <Card className="rounded-xs bg-[#eaeaea] p-20 border-0 py-0">
+                <CardContent className="flex relative h-100 items-center justify-center p-6">
                   <Image
                     src={"/images/image1.png"}
                     alt="product image"
                     fill
                   />
 
-                  {index !== 0 && (
+                  {/* {index !== 0 && (
                     <div className="bg-zinc-0 absolute w-full h-full bg-clip-padding backdrop-filter backdrop-blur-sm " />
-                  )}
+                  )} */}
                 </CardContent>
               </Card>
           </CarouselItem>
