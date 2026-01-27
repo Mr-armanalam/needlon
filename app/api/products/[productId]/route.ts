08e420c122ef9a8026cdc8e1ea3b96cd6809abaa
Arman Alam <armanalam78578@gmail.com>
2026-01-27 07:20:47 +0530

    filter category logic completed for the individual product

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -1,5 +1,8 @@
 import { db } from "@/db";
+import { filterGroups } from "@/db/schema/filter-group";
+import { filterOptions } from "@/db/schema/filter-options";
 import { productCategory } from "@/db/schema/product-category";
+import { productFilterOptions } from "@/db/schema/product-filter-options";
 import { productItems } from "@/db/schema/product-items";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
@@ -11,7 +14,8 @@ export const GET = async (
   try {
     const { productId } = await params;
 
-    if (!productId) return NextResponse.json({ productItem: null }, { status: 404 });
+    if (!productId)
+      return NextResponse.json({ productItem: null }, { status: 404 });
 
     const [product] = await db
       .select()
@@ -22,7 +26,27 @@ export const GET = async (
         eq(productCategory.id, productItems.categoryId),
       );
 
-    if (!product) return NextResponse.json({ productItem: null }, { status: 500 });
+    const filter = await db
+      .select()
+      .from(productFilterOptions)
+      .where(eq(productFilterOptions.productId, product.product_items.id))
+      .innerJoin(
+        filterOptions,
+        eq(filterOptions.id, productFilterOptions.filterOptionId),
+      )
+      .innerJoin(
+        filterGroups,
+        eq(filterGroups.id, filterOptions.filterGroupId),
+      );
+
+    const productFilterData = filter.map((data) => ({
+      [data.filter_groups.name]: data.filter_options.value,
+    }));
+
+    console.log(productFilterData);
+
+    if (!product)
+      return NextResponse.json({ productItem: null }, { status: 500 });
 
     return NextResponse.json({ productItem: product }, { status: 200 });
   } catch (error) {
