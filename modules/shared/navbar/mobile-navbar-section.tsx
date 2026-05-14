a83f3d45beabfdd3326128ce5dcda18c1642b78e
Arman Alam <armanalam78578@gmail.com>
2026-05-14 22:01:54 +0530

    feat: implement mobile navbar with improved layout and responsiveness

--- modules/shared/navbar/mobile-navbar-section.tsx
+++ modules/shared/navbar/mobile-navbar-section.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { GoHome } from "react-icons/go";
+import { PiSquaresFour } from "react-icons/pi";
+import { FaRegUser } from "react-icons/fa";
+import { BsCart2 } from "react-icons/bs";
+
+const mobileNavData = [
+  {
+    label: 'Home',
+    icon: GoHome,
+    link: '/'
+  },
+  {
+    label: 'Categories',
+    icon: PiSquaresFour,
+    link: '/m-categories'
+  },
+  {
+    label: 'Account',
+    icon: FaRegUser,
+    link: '/m-account'
+  },
+  {
+    label: 'Cart',
+    icon: BsCart2,
+    link: '/cart'
+  },
+]
+
 
 const MobileNavbarSection = () => {
   return (
-    <div className='xl:hidden sticky bottom-0 right-0 mt-auto h-12 flex items-center justify-center bg-stone-200 z-50  left-0'>MobileNavbarSection</div>
+    <div className='xl:hidden sticky bottom-0 right-0 mt-auto px-8 py-3 h-fit flex items-center justify-between bg-white z-50 border-t left-0'>
+      {mobileNavData.map((item, index) => (
+        <div key={index} className='flex items-center text-stone-600 flex-col'>
+          <item.icon size={item.label === 'Account' ? 28 : 30 } />
+          <p className='text-xs'>{item.label}</p>
+        </div>
+        ))}
+    </div>
   )
 }
 
